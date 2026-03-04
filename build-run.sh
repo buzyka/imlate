5497b942f05a2e16e215f04f27b25ba765b89932
Viktor Buzyka <v.buzyka@shopware.com>
2026-03-04 15:59:11 +0100

    feat: add administration FE

--- build-run.sh
+++ build-run.sh
@@ -2,9 +2,15 @@
 
 CGO_ENABLED=1 go run cmd/app/main.go
 
+
+
 # CGO_ENABLED=1 go build -o tracker.exe cmd/app/main.go
 
 # CGO_ENABLED=1 GOOS=windows GOARCH=amd64 CC=x86_64-w64-mingw32-gcc CXX=x86_64-w64-mingw32-g++ go build -o tracker.exe cmd/app/main.go
+
+
+#linux
+# GOOS=linux GOARCH=amd64 go build -o app cmd/app/main.go
 
 
 # migrations
